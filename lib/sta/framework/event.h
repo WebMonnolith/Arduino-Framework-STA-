b0276a2fb9a057a9fe63371810de154cd8f0109a
JVP11292021 <jessyvanpolanen@gmail.com>
2024-01-06 17:52:37 +0100

    first prototype of the different events

--- lib/sta/framework/event.h
+++ lib/sta/framework/event.h
@@ -1,26 +1,30 @@
-#ifndef _STA_EVENT_H_
-#define _STA_EVENT_H_
+#ifndef _STA_event_H_
+#define _STA_event_H_
 
 #include "./core/functional.h"
+#include "./core/exception.h"
 
 #define STA_BIT(x) 						(1 << (x))
 #define STA_LSB(x)						((x) ^ ((x) - 1) & (x))
 
 // Macros for simplifying the abstract method implementations
 
-#define LGE_EVENT_CLASS_TYPE(type)  static sta::EventType getStaticType() { return sta::EventType::##type; }\
-									virtual sta::EventType getEventType() const override { return getStaticType(); }\
+#define STA_EVENT_CLASS_TYPE(type)  static sta::event_type getStaticType() { return sta::##type; }\
+									virtual sta::event_type getevent_type() const override { return getStaticType(); }\
 									virtual const char* getName() const override { return #type; }
 
-#define LGE_EVENT_CLASS_CATEGORY(category) virtual sta::int32 getCategoryFlags() const override {return category;}
+#define STA_EVENT_CLASS_CATEGORY(category) virtual sta::int32 getCategoryFlags() const override {return category;}
 
 BEGIN_NP_BLOCK
 
-_STAXXEXPORT enum class EventType {
+_STAXXEXPORT enum class event_type {
 	none = 0,
+	appTick, appUpdate, appError, appException,
+	serverStatus
+
 };
 
-_STAXXEXPORT enum EventCategory {
+_STAXXEXPORT enum event_category {
 	none = 0,
 	eventCategoryException  		= STA_BIT(0),
 	eventCategoryInput				= STA_BIT(1),
@@ -28,42 +32,41 @@ _STAXXEXPORT enum EventCategory {
 };
 
 
-class _STAXXEXPORT Event {
+class _STAXXEXPORT event {
 private:
-	friend class EventDispatcher;
+	friend class event_dispatcher;
 	int32 handled = 0;
 public:
-    Event(const Event& event) {
-
-    }
+	event() = default;
+    event(const event& event) {}
 public:
 
-	virtual ~Event() = default;
+	virtual ~event() = default;
 
-	virtual EventType getEventType() const = 0;
+	virtual event_type getevent_type() const = 0;
 	virtual const char* getName() const = 0;
 	virtual int32 getCategoryFlags() const = 0;
 	virtual String toString() const { return String(this->getName()); } // DEBUG only
 
 public:
-	inline int32 isInCategory(EventCategory category) { return this->getCategoryFlags() & category; }
+	inline int32 isInCategory(event_category category) { return this->getCategoryFlags() & category; }
 	inline int32 isHandled() const { return this->handled; }
 };
 
-class _STAXXEXPORT EventDispatcher {
+class _STAXXEXPORT event_dispatcher {
 private:
-	Event& event;
+	event& e;
 
 	template <typename T>
-	using EventFn = sta::function<bool(T&)>;
+	using eventFn = sta::function<bool(T&)>;
 
 public:
 
-	EventDispatcher(Event& event) : event(event) {}
+	event_dispatcher(event& e) : e(e) {}
 
 	template <typename T>
-	int32 dispatch(EventFn<T> func) {
-		if (this->event.getEventType() == T::getStaticType()) {
+	int32 dispatch(eventFn<T> func) {
+		if (this->event.getevent_type() == T::getStaticType()) {
 			this->event.handled = func(*(T*)&this->event);
 			return 1;
 		}
@@ -71,9 +74,39 @@ public:
 	}
 };
 
-class _STAXXEXPORT ExceptionEvent {
-    
-}
+class _STAXXEXPORT exception_event : public event {
+public:
+	exception_event(exception e)
+		: ex(e) 
+	{}
+public:
+	STA_EVENT_CLASS_TYPE(event_type::appException)
+	STA_EVENT_CLASS_CATEGORY(event_category::eventCategoryException)
+private:
+	exception ex;
+};
+
+class _STAXXEXPORT hid_event : public event {
+public:
+	hid_event(int32 value)
+		: value(value) 
+	{}
+public:
+	STA_EVENT_CLASS_TYPE(event_type::appUpdate)
+	STA_EVENT_CLASS_CATEGORY(event_category::eventCategoryInput)
+private:
+	int32 value;
+};
+
+class _STAXXEXPORT server_event : public event {
+public:
+	server_event()
+	{}
+public:
+	STA_EVENT_CLASS_TYPE(event_type::serverStatus)
+	STA_EVENT_CLASS_CATEGORY(event_category::eventCategoryServer)
+private:
+};
 
 END_NP_BLOCK
 
