3d170efaa4e29c6356e2a62ddf13103d6eebc4ae
JVP11292021 <jessyvanpolanen@gmail.com>
2024-03-06 13:45:00 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,24 @@
 #include <Arduino.h>
 #include <sta.h>
 
-#include <HTTPClient.h>
+sta::pair<sta::function<int()> , sta::function<int()>> closure(int x) {
+  auto fn1 = [x]() mutable {
+    x++;
+    return x;
+  };
+
+  auto fn2 = [x]() mutable {
+    x--;
+    return x;
+  };
+
+  return sta::pair<sta::function<int()> , sta::function<int()>>(fn1, fn2);
+}
+
+template <typename T>
+void foo(T i) {
+  Serial.println(i);
+}
 
 using namespace sta::literals;
 using namespace sta::control;
@@ -9,7 +26,7 @@ using namespace sta::control;
 class Controller : public sta::micro_controller {
 public:
   Controller() {
-    sta::request<int> req = sta::post_request<int>();
+    
   }
 
   Controller(const Controller&) = default;
@@ -27,7 +44,9 @@ public:
   }
 
   bool onUpdate() override {
-
+    // sta::logln(closure_fn.first());
+    foo("Hello world");
+    foo(7);
     return true;
   }
 
@@ -37,6 +56,9 @@ public:
 private:
   int counterVar = 0;
   unsigned long long previous = 0_sdp;
+
+  sta::pair<sta::function<int()> , sta::function<int()>> closure_fn = closure(8);
+
 };
 
 sta::micro_controller* sta::create_app() {
